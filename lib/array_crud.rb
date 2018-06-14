c454593f564bdde34d2fd727aa36cbe6b179b0ec
Andrew Woodward <ebenwoodward@icloud.com>
2018-06-14 18:12:32 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,5 +1,5 @@
 def create_an_empty_array
-  
+  empty_arr = []
 end
 
 def create_an_array
@@ -7,19 +7,19 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  end_arr = array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  start_arr = array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  remove_end = array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift 
 end
 
 def retrieve_element_from_index(array, index_number)
